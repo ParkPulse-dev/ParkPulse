fa43a635a6fb78406ff75f7da1897fd3f7ae9cc2
Aviv Shevach <shevach.aviv@gmail.com>
2024-03-17 02:55:26 +0200

    popup

--- Assets/Scripts/startTutorial.cs
+++ Assets/Scripts/startTutorial.cs
@@ -2,24 +2,20 @@ using UnityEngine;
 
 public class startTutorial : MonoBehaviour
 {
-    public GameObject popupObject; // קישור לאובייקט של ה-popup
+    public GameObject popupObject;
 
     void Start()
     {
-        // עצור את המשחק במצב pause
-        
 
-        // הפעל את ה-popup
-      //  popupObject.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void ClosePopup()
     {
-        // הפעל את המשחק מחדש
+
         Time.timeScale = 1;
 
-        // סגור את ה-popup
+
         popupObject.SetActive(false);
     }
 }
